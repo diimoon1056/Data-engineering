9b94d1ad73350df0e0733a2cd0da25409d35b054
diimoon1056 <167615525+diimoon1056@users.noreply.github.com>
2025-02-03 15:47:06 +0200

    Create practical.sql

---
+++ Practical4/practical.sql
@@ -0,0 +1,23 @@
+create database University;
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100),
+    birth_date DATE
+);
+
+CREATE TABLE Courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100),
+    credits INT
+);
+
+CREATE TABLE Enrollments (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    enrollment_date DATE,
+    FOREIGN KEY (student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
