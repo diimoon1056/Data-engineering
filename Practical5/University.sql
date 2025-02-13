b3adb01e684d6c77469b35423191ab450a5f9de6
diimoon1056 <167615525+diimoon1056@users.noreply.github.com>
2025-02-13 02:16:34 +0200

    Update University.sql

--- Practical5/University.sql
+++ Practical5/University.sql
@@ -1,5 +1,6 @@
-create database University;
-
+--Завдання 1
+create database Shop;
+USE Shop;
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(100) NOT NULL,
@@ -66,4 +67,86 @@ VALUES
 (9, 'Groceries'),
 (10, 'Automotive');
 
-INSERT INTO orders()
+INSERT INTO Orders (customer_id, order_date, total_price) VALUES
+(1, '2025-02-01', 250.75),
+(2, '2025-02-02', 120.40),
+(3, '2025-02-03', 315.00),
+(4, '2025-02-04', 540.60),
+(5, '2025-02-05', 95.20),
+(6, '2025-02-06', 425.30),
+(7, '2025-02-07', 210.50),
+(8, '2025-02-08', 800.90),
+(9, '2025-02-09', 150.00),
+(10, '2025-02-10', 400.00);
+
+--Завдання 2
+
+SELECT SUM(total_price) AS total_orders_sum
+FROM Orders;
+
+SELECT customer_id, COUNT(order_id) AS order_count
+FROM Orders
+GROUP BY customer_id;
+
+INSERT INTO Products (product_name, category_id, price, stock_quantity) VALUES
+('Laptop', 1, 1200.99, 50),
+('Smartphone', 2, 699.99, 100),
+('Headphones', 3, 199.50, 150),
+('Keyboard', 4, 89.99, 200),
+('Mouse', 4, 39.99, 300),
+('Monitor', 1, 300.75, 80),
+('Smartwatch', 2, 250.00, 120),
+('Tablet', 1, 499.99, 60),
+('Speakers', 3, 150.00, 180),
+('Camera', 2, 450.00, 70);
+
+SELECT 
+    MIN(price) AS min_price,
+    MAX(price) AS max_price,
+    AVG(price) AS avg_price
+FROM Products;
+
+INSERT INTO OrderDetails (order_id, product_id, quantity, price) VALUES
+(1, 1, 2, 1200.99),
+(1, 2, 1, 699.99),
+(2, 3, 3, 199.50),
+(3, 4, 1, 89.99),
+(4, 5, 5, 39.99),
+(5, 6, 2, 300.75),
+(6, 7, 1, 250.00),
+(7, 8, 4, 499.99),
+(8, 9, 1, 150.00),
+(9, 10, 54534, 450.00);
+
+SELECT p.category_id, SUM(od.quantity) AS total_sold
+FROM Products p
+JOIN OrderDetails od ON p.product_id = od.product_id
+GROUP BY p.category_id;
+
+SELECT p.category_id, SUM(od.quantity) AS total_sold
+FROM Products p
+JOIN OrderDetails od ON p.product_id = od.product_id
+GROUP BY p.category_id
+HAVING SUM(od.quantity) > 100;
+
+--Завдання 3
+
+SELECT order_id, total_price
+FROM Orders
+WHERE total_price > (SELECT AVG(total_price) FROM Orders);
+
+SELECT customer_id
+FROM Customers
+WHERE customer_id IN (SELECT DISTINCT customer_id FROM Orders);
+
+SELECT product_id
+FROM Products
+WHERE product_id NOT IN (SELECT DISTINCT product_id FROM OrderDetails);
+
+SELECT p.category_id, p.product_id, p.product_name, p.price
+FROM Products p
+WHERE p.price = (
+    SELECT MAX(p2.price)
+    FROM Products p2
+    WHERE p2.category_id = p.category_id
+);
